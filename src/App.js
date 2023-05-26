f7900771dd89b0fb9f22596d1da404cce4303e18
ukrwebprom <ukrwebprom@gmail.com>
2023-05-26 21:19:18 +0300

    button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import { Button } from './components/Botton/Button';
 function App() {
+
+  const tg = window.Telegram.WebApp;
   return (
     <div className="App">
       work
+      <Button>text</Button>
     </div>
   );
 }
---
+++ src/components/Botton/Button.jsx
@@ -0,0 +1,7 @@
+import './button.css';
+
+export const Button = (props) => {
+    return(
+        <button {...props} type="button" className='btn' />
+    )
+}
