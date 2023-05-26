d6d3004174a3c15747d4830f8e9c0dadbf861533
ukrwebprom <ukrwebprom@gmail.com>
2023-05-26 22:02:39 +0300

    mainbtn

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -3,9 +3,9 @@ const tg = window.Telegram.WebApp;
 export const useTelegram = () => {
     const onTobbleButton = () => {
       if(tg.MainButton.isVisible) {
-        tg.MainButton.show();
-      } else {
         tg.MainButton.hide();
+      } else {
+        tg.MainButton.show();
       }
     }
     return {
