53cdf2fb45cb0f4a3605dc509837c50a28710f6e
ukrwebprom <ukrwebprom@gmail.com>
2023-05-27 02:10:20 +0300

    mainpage sections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import './home.css';
-
+import { Link } from "react-router-dom";
 export const Home = () => {
     return(
         <div className="home-container">
-            
+            <button type="button" className='close-btn'></button>
+            <h1 className='main-title'>Yurii Pochtiennykh</h1>
+            <ul className='expertise-list'>
+                <li>Junior Full-Stack Developer</li>
+                <li>Designer</li>
+            </ul>
+            <section className='section-one'>
+            <p className='section-text'>I'm always looking for opportunities to enhance my skills and knowledge, and my last win is Front End developments expertise...</p>
+            <Link className='read-more'>Read more</Link>
+            </section>
         </div>
     )
 }
