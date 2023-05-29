c446172164d12180687bf5a0a3a1f9999d0fffc5
ukrwebprom <ukrwebprom@gmail.com>
2023-05-29 15:18:26 +0300

    design update1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,7 +16,7 @@ export const Home = () => {
             knowledge, and my last win is Full-Stack Dev expertise...
           </p>
         </div>
-        <p>Skills list</p>
+        <Link to='skills' className="skills-link">Skills list</Link>
       </section>
       <section className="section section-three">
         <div className="section-header">
