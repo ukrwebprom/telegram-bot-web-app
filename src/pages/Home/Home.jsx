a2eb779063aa8ba6a22d35bcbee7cfca7fb48abc
ukrwebprom <ukrwebprom@gmail.com>
2023-05-29 23:04:58 +0300

    text block

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,6 +28,15 @@ export const Home = () => {
           <a href="https://ukrwebprom.github.io/websocket-chat/" className="try-it-out">Try it out</a>
         </div>
       </section>
+      <section className="text-section">
+      <h2>why me?</h2>
+      <strong>I'm a junior developer but I'm not new to IT and I know how it works.</strong>
+      <p>I have been involved in the creation of several startups and I know how great systems come from a simple idea.</p>
+      <p>I worked as a web designer, I am fluent in design programs and I know how a developer interacts with a designer.</p>
+      <p>I was a project manager and I know how to communicate with clients.</p>
+      <strong>Now I want to try my hand at writing code.</strong>
+      <a>Yurii Pochtiennykh (.pdf)</a>
+      </section>
 {/*       <section className="section section-three">
         <div className="section-header">
           <h2>The latest project</h2>
