a02ac6e5f16c0eb72774d47dc54ed4bbd5afc592
ukrwebprom <ukrwebprom@gmail.com>
2023-05-29 23:49:37 +0300

    Update Skills.jsx

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,7 +1,33 @@
 export const Skills = () => {
     return(
         <div>
-            Skills
+            <h2>hard skills</h2>
+            <ul>
+                <li>HTML5</li>
+                <li>CSS/SASS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Redux</li>
+                <li>Node.js</li>
+            </ul>
+            <h2>medium skills</h2>
+            <ul>
+                <li>Responsive / Adaptive design</li>
+                <li>REST API</li>
+                <li>Websockets</li>
+                <li>Firebase</li>
+                <li>Git</li>
+                <li>Extensive skills in design area</li>
+            </ul>
+            <h2>soft skills</h2>
+            <ul>
+                <li>Friendliness</li>
+                <li>Self-motivation</li>
+                <li>Fast learner</li>
+                <li>Meeting deadlines</li>
+                <li>Focus on the task</li>
+                <li>Nine to five mentality</li>
+            </ul>
         </div>
     )
 }
