e4b7da46b54979e570ca1e369852ca5f60a3f04d
ukrwebprom <ukrwebprom@gmail.com>
2023-05-30 10:56:32 +0300

    Update Contacts.jsx

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -3,10 +3,12 @@ import { ContactItem } from "../../components/ContactItem/ContactItem"
 export const Contacts = () => {
     return(
         <div>
+            <h2 >contacts & links</h2>
             <ContactItem name={'Location'}>Odessa, Ukraine</ContactItem>
             <ContactItem name={'Phone'}>+380 96 333 6533</ContactItem>
             <ContactItem name={'e-mail'}>ukrwebprom@gmail.com</ContactItem>
             <ContactItem name={'Linkedin'}><a href='https://www.linkedin.com/in/yuri-pochtennykh/'>yuri-pochtennykh</a></ContactItem>
+            <ContactItem name={'Github'}><a href='https://github.com/ukrwebprom'>ukrwebprom</a></ContactItem>
             <ContactItem name={'Portfolio'}><a href='https://www.behance.net/ukrwebprom'>my design works on Behance</a></ContactItem>
         </div>
     )
